ddd18da4a4ec29ece83fbd02d50cc88da8b1b64b
Grant Magella <Bitman2003@gmail.com>
2023-09-25 09:13:42 -0400

    Intial COmmit

---
+++ src/main/java/com/maxtraining/c40/Plinko/PlinkoApplication.java
@@ -0,0 +1,13 @@
+package com.maxtraining.c40.Plinko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlinkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlinkoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxtraining/c40/Plinko/PlinkoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxtraining.c40.Plinko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlinkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
